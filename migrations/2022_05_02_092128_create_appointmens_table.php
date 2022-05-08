86a4f4c1842b19b1f7c52346a08f982333f80e03
putridwi24 <putri.119140068@student.itera.ac.id>
2022-05-08 17:30:29 +0700

    database app

---
+++ migrations/2022_05_02_092128_create_appointmens_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama')->nullable();
+            $table->string('email')->nullable();
+            $table->string('telepon')->nullable();
+            $table->string('dokter')->nullable();
+            $table->string('tanggal')->nullable();
+            $table->string('pesan')->nullable();
+            $table->string('status')->nullable();
+            $table->string('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
